7d502cbcb605d057d57cd1706eeb94ef0e40546a
Cristian Iturri <iturri.cf@gmail.com>
2023-05-08 01:29:45 -0300

    Export both sui native and known tokens

--- src/wallets/sui/sui.config.ts
+++ src/wallets/sui/sui.config.ts
@@ -1,10 +1,10 @@
 import { DEVNET } from "..";
 
-export const SUI = 'sui';
+export const SUI = "sui";
 
-const SUI_TESTNET = 'testnet';
+const SUI_TESTNET = "testnet";
 
-const SUI_MAINNET = 'mainnet';
+export const SUI_MAINNET = "mainnet";
 
 export type SuiDefaultConfig = {
   nodeUrl: string;
@@ -20,23 +20,28 @@ export type SuiDefaultConfigs = Record<string, SuiDefaultConfig>;
 
 const SUI_DEFAULT_CONFIGS: SuiDefaultConfigs = {
   [DEVNET]: {
-    nodeUrl: 'http://localhost:8545', // ??
+    nodeUrl: "http://localhost:8545", // ??
   },
   [SUI_TESTNET]: {
-    nodeUrl: 'https://fullnode.testnet.sui.io'
+    nodeUrl: "https://fullnode.testnet.sui.io",
   },
   [SUI_MAINNET]: {
-    nodeUrl: 'https://sui-mainnet-rpc.allthatnode.com',
-  }
+    nodeUrl: "https://sui-mainnet-rpc.allthatnode.com",
+  },
 };
 
+export const SUI_NATIVE_COIN_MODULE = "0x2::sui::SUI";
+
+export const SUI_KNOWN_TOKENS = {
+  [SUI_MAINNET]: {
+    USDC: "0x5d4b302506645c37ff133b98c4b50a5ae14841659738d6d733d59d0d217a93bf::coin::COIN",
+  },
+};
 export const SUI_CHAIN_CONFIG = {
   chainName: SUI,
-  nativeCurrencySymbol: 'SUI',
-  knownTokens: {},
+  nativeCurrencySymbol: "SUI",
+  knownTokens: SUI_KNOWN_TOKENS,
   defaultConfigs: SUI_DEFAULT_CONFIGS,
   networks: SUI_NETWORKS,
   defaultNetwork: SUI_TESTNET,
 };
-
-export const SUI_NATIVE_COIN_MODULE = "0x2::sui::SUI";
