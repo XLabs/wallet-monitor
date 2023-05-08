fe34c31269b943f903226a07232b2d7b444bebe2
Cristian Iturri <iturri.cf@gmail.com>
2023-05-08 14:55:08 -0300

    Add test case for max uint64. Do not run concurrently

--- src/balances/sui.test.ts
+++ src/balances/sui.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, test } from '@jest/globals';
 import { formatRawUnits } from '.';
 
 describe("Test sui balance util", () => {
-    test.concurrent.each([
+    test.each([
         ["100000000", 6, "100.0"], // 100 usdc (usdc has 6 decimals)
         ["1000000000", 9, "1.0"], // 1 sui (sui has 9 decimals)
         ["10000000000", 9, "10.0"], // 10 sui
@@ -14,7 +14,8 @@ describe("Test sui balance util", () => {
         ["-1", 9, "-0.000000001"], // negative minimal precision for sui
         ["-9950000", 6, "-9.95"], // negative usdc
         ["-1", 6, "-0.000001"], // negative minimal precision for usdc
-    ])("Test format units", async (rawUnits: string, decimals: number, expectedFormattedValue: string) => {
+        [(2n ** 64n - 1n).toString(), 8, "184467440737.09551615"], // max uint64
+    ])("Test format units", (rawUnits: string, decimals: number, expectedFormattedValue: string) => {
         let result = formatRawUnits(rawUnits, decimals);
         expect(result).toBe(expectedFormattedValue);
     });
