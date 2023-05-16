678135eee47694cb23c2bda56a1cc68faf2ebdcb
CiottiGiorgio <giorgio@xlabs.xyz>
2023-05-16 11:32:41 +0200

    Removed unnecessary code comments

--- src/i-wallet-manager.ts
+++ src/i-wallet-manager.ts
@@ -15,30 +15,10 @@ Goals for this interface file:
    attempt at doing them concurrently. They are better suited in the image when multiple RE instances are up and
    therefore in that case RE would only need to see a component implementing IWMLocks interface (which will
    hook into the gRPC service with a client).
-THE FOLLOWING POINT IS LEFT HERE FOR HISTORIC DOCUMENTATION UNTIL THE NEXT ITERATION IS READY
-- The gRPC service is using bare acquire/release WalletManager calls because it is tying them to acquire/release requests.
-   Ideally, IWMLocks should only be .withWallet(). There's no use case for the RE using those bare calls outside
-   the context of a relaying job. Also, this would put the business logic of throwing an error immediately if the gRPC
-   service crashes and not waiting until the release request is sent to figure out that the service crashed.
-   This would necessarily need to be implemented as a long-lived unary request. When the request ends, the lock is released.
-   I'm not sure if gRPC can handle this kind of thing with heartbeats.
-   If this is possible, it would be awesome to replicate a python context manager implementation where the relaying job
-   is aborted/rolled back if the lock is lost before releasing.
-- This point overrides the preceding one. The problem with a single long-lived unary request is that it still does not
-   guarantee persistence of the state of the locks. It also does not play nicely with live upgrades.
-   Figuring out if the service has crashed could be achieved by inspecting the state of the gRPC channel (not the gRPC request).
-   If that connection is not alive anymore, rollback all current jobs. We would still like to only have .withWallet
-   on the local hook and bare acquire/release on the WalletManager used within the gRPC service.
-   We can do this by having two different IWMLock interfaces (one with bare methods, the other with .withWallet()).
-   Then, two different IWalletManager interfaces extending from each IWMLock interface.
-   WalletManager class should implement everything but only the things behind the interface are available to the
-   code that is instantiating it.
 - The only exported interface that should actually be for a different class, is IClientWalletManager.
    The class implementing this will obviously just be a client for the remote gRPC service.
    Potentially a WalletManager implements IClientWalletManager, so it's care of the dev/library to make sure that
    the appropriate class is used in each circumstance.
-- To avoid having the user knowing about this fact and loading them with the burden of knowing all interfaces and classes,
-   we should provide convenience factory functions. TODO
 */
 
 
