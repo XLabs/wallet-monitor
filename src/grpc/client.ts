82faa23913a605097b14587d4b1e40e7a3049d07
CiottiGiorgio <giorgio@xlabs.xyz>
2023-05-17 11:58:52 +0200

    Migrated client code to nice-grpc and ts-proto.

--- src/grpc/client.ts
+++ src/grpc/client.ts
@@ -1,16 +1,17 @@
 import {ChainWalletManager, WalletExecuteOptions, WithWalletExecutor} from "../chain-wallet-manager";
 import {ChainName, isChain} from "../wallets";
-import * as grpc from '@grpc/grpc-js';
 import {getDefaultNetwork, WalletManagerConfig, WalletManagerOptions} from "../wallet-manager";
 import winston from "winston";
 import {createLogger} from "../utils";
-import * as util from "util";
 import {IClientWalletManager} from "../i-wallet-manager";
-const { WalletManagerClient: WalletManagerProtocol } = require('./out/wallet-manager_grpc_pb');
-const { AcquireLockRequest, ReleaseLockRequest } = require('./out/wallet-manager_pb')
+import {createChannel, createClient} from "nice-grpc";
+import {
+    WalletManagerGRPCServiceDefinition
+} from "./out/wallet-manager-grpc-service";
 
 export class ClientWalletManager implements IClientWalletManager {
-    private grpcClient;
+    private walletManagerGRPCChannel;
+    private walletManagerGRPCClient;
     private managers;
 
     protected logger: winston.Logger;
@@ -19,7 +20,9 @@ export class ClientWalletManager implements IClientWalletManager {
         this.logger = createLogger(options?.logger, options?.logLevel, { label: 'WalletManager' });
         this.managers = {} as Record<ChainName, ChainWalletManager>;
 
-        this.grpcClient = new WalletManagerProtocol(`${host}:${port}`, grpc.credentials.createInsecure())
+        // this.walletManagerGRPCClient = new WalletManagerProtocol(`${host}:${port}`, grpc.credentials.createInsecure())
+        this.walletManagerGRPCChannel = createChannel(`${host}:${port}`)
+        this.walletManagerGRPCClient = createClient(WalletManagerGRPCServiceDefinition, this.walletManagerGRPCChannel)
 
         // Constructing a record of manager for the only purpose of extracting the appropriate provider and private key
         //  to bundle together with the lock acquired from the grpc service.
@@ -39,20 +42,11 @@ export class ClientWalletManager implements IClientWalletManager {
         }
     }
 
-    // FIXME: Too much promisifying in this code. It would be better to do it only once or, ideally, generate
-    //  code that is using async syntax out of the box.
     public async withWallet(chainName: ChainName, fn: WithWalletExecutor, opts?: WalletExecuteOptions): Promise<void> {
         const chainManager = this.managers[chainName];
         if (!chainManager) throw new Error(`No wallets configured for chain: ${chainName}`);
 
-        const acquireRequest = new AcquireLockRequest();
-        acquireRequest.setChainName(chainName);
-        opts?.address ? acquireRequest.setAddress(opts.address) : acquireRequest.clearAddress();
-        opts?.leaseTimeout ? acquireRequest.setLeaseTimeout(opts.leaseTimeout) : acquireRequest.clearLeaseTimeout();
-
-        const promiseAcquireLock = util.promisify(this.grpcClient.acquireLock).bind(this.grpcClient);
-        const acquireResponse = await promiseAcquireLock(acquireRequest);
-        const acquiredAddress = acquireResponse.getAddress();
+        const { address: acquiredAddress } = await this.walletManagerGRPCClient.acquireLock({chainName, address: opts?.address, leaseTimeout: opts?.leaseTimeout})
 
         // FIXME
         // Dirty solution. We are doing as little work as possible to get the same expected WalletInterface after
@@ -68,14 +62,8 @@ export class ClientWalletManager implements IClientWalletManager {
             this.logger.error('The workflow function failed to run within the context of the acquired wallet.', error);
             throw error;
         } finally {
-            const releaseRequest = new ReleaseLockRequest();
-            releaseRequest.setChainName(chainName);
-            releaseRequest.setAddress(acquiredAddress);
-
-            const promiseReleaseLock = util.promisify(this.grpcClient.releaseLock).bind(this.grpcClient);
-
             await Promise.all([
-                promiseReleaseLock(releaseRequest),
+                this.walletManagerGRPCClient.releaseLock({chainName, address: acquiredAddress}),
                 this.managers[chainName].releaseLock(acquiredAddress)
             ]);
         }
