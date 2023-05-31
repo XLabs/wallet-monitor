cf4f9a3ca8ca732b93393481f85d83abbed4738e
solanoe <solanoepalacio@gmail.com>
2023-05-31 11:26:48 -0300

    fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,17 @@
 
 export type { ILibraryWalletManager, IServiceWalletManager, IClientWalletManager } from './i-wallet-manager'
+export type { WalletManagerFullConfig } from './wallet-manager'
 export { buildWalletManager } from './utils'
 
-import { EVM_CHAIN_CONFIGS  } from './wallets/evm';
+import { EVM_CHAIN_CONFIGS } from './wallets/evm';
 // import { SOLANA_CHAINS } from './wallets/solana';
-
 import {
-    WalletManagerOptions as WMOptions,
-    WalletManagerConfig as WMConfig,
-} from './wallet-manager';
-import {
-    WalletBalancesByAddress as WBBA,
+  WalletBalancesByAddress as WBBA,
 } from './chain-wallet-manager';
-import {
-    WalletInterface as WI,
-} from './wallets/base-wallet';
 
-export type WalletManagerOptions = WMOptions;
-export type WalletManagerConfig = WMConfig;
+import {
+  WalletInterface as WI,
+} from './wallets/base-wallet';
 export type WalletBalancesByAddress = WBBA;
 export type WalletInterface = WI;
 
@@ -28,8 +22,8 @@ export type WalletInterface = WI;
  * but this map contains available networks for each chain just in case you want to be 100% sure.
  */
 export const NETWORKS = {
-    [EVM_CHAIN_CONFIGS.ethereum.chainName]: EVM_CHAIN_CONFIGS.ethereum.networks,
-    [EVM_CHAIN_CONFIGS.polygon.chainName]: EVM_CHAIN_CONFIGS.polygon.networks,
-    [EVM_CHAIN_CONFIGS.avalanche.chainName]: EVM_CHAIN_CONFIGS.avalanche.networks,
+  [EVM_CHAIN_CONFIGS.ethereum.chainName]: EVM_CHAIN_CONFIGS.ethereum.networks,
+  [EVM_CHAIN_CONFIGS.polygon.chainName]: EVM_CHAIN_CONFIGS.polygon.networks,
+  [EVM_CHAIN_CONFIGS.avalanche.chainName]: EVM_CHAIN_CONFIGS.avalanche.networks,
 };
 
